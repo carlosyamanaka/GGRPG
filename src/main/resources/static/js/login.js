ea60eaebcf0b3d68f14ad2dfc389d6bc0dd8d7a9
carlosyamanaka <109562378+carlosyamanaka@users.noreply.github.com>
2023-10-26 19:49:01 -0300

    login com firebase implementado na versao inicial

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,12 +1,26 @@
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCBFJia4j06OH8ZBhTzbju6yaVby-2cEik",
+    authDomain: "ggrpg-2d938.firebaseapp.com",
+    databaseURL: "https://ggrpg-2d938-default-rtdb.firebaseio.com",
+    projectId: "ggrpg-2d938",
+    storageBucket: "ggrpg-2d938.appspot.com",
+    messagingSenderId: "840757499698",
+    appId: "1:840757499698:web:716f4263d78adeb1e5292b",
+    measurementId: "G-KE5EDQ3443"
+};  
+firebase.initializeApp(firebaseConfig);
+
 
 function login() {
-    //Promisses sao assincronas
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
     firebase.auth().signInWithEmailAndPassword(
-        form.email().value, form.password().value
-    ).then(response => {
-            window.location.href = "../html/index.html"
+        email, senha
+    ).then((response) => {
+            console.log("sucesso", response);
+            window.location.href = "index.html"
         }).catch(error => {
             console.log("error", error);
         })
